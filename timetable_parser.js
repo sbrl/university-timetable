4e038e621ba87762c42eec0b4a8b9d6ac098a866
Starbeamrainbowlabs <sbrl@starbeamrainbowlabs.com>
2017-09-13 15:35:40 +0100

    Bugfix timetable html parser

--- timetable_parser.js
+++ timetable_parser.js
@@ -4,17 +4,17 @@ const crypto = require("crypto");
 
 const cheerio = require("cheerio");
 const moment = require("moment");
-const ical_toolkit = require("ical_toolkit");
+const ical_toolkit = require("ical-toolkit");
 
 class TimetableParser
 {
 	constructor(timetableHTML)
 	{
-		this.cheerio_doc = cheerio.load(timetableHTML);
+		this.doc = cheerio.load(timetableHTML);
 		
 		this.events = [];
 		
-		this.week1 = moment({ year: (new Date()).getFullYear() }).week(35);
+		this.week1 = moment({ year: (new Date()).getFullYear() }).week(35 - 1);
 		
 		this.calendar = ical_toolkit.createIcsFileBuilder();
 		this.calendar.thowError = true;
@@ -29,31 +29,52 @@ class TimetableParser
 	 */
 	asIcal()
 	{
+		// Parse out the html and extract the events
 		this.parse_events();
 		
+		console.log(`Week 1: ${this.week1}`)
+		
+		// Convert the left-over moment objects to vanilla Date objects
+		this.calendar.events.sort(function(a, b) {
+			if(a.start.isSame(b.start, "seconds"))
+				return 0;
+			if(a.start.isSameOrAfter(b.start))
+				return 1;
+			return -1;
+		});
+		this.calendar.events.forEach((ev) => {
+			ev.start = ev.start.toDate();
+			ev.end = ev.end.toDate();
+			
+			console.log(`[ ${ev.start.toString()} - ${ev.start.toString()} ] ${ev.summary} @ ${ev.location} with ${ev.organizer.name}`);
+		});
+		
+		// Convert the calendar to ics
 		return this.calendar.toString();
 	}
 	
 	parse_events()
 	{
-		this.cheerio_doc(".spreadsheet tr:not(.columnTitles)").each((function(i, tr) {
-			let event_summary = `${tr.children()[2].text().trim()} ${tr.children()[3].text().trim()}`,
-				event_location = tr.children()[8].text().trim(),
+		this.doc(".spreadsheet tr:not(.columnTitles)").each((function(i, tr) {
+			let tr_fields = this.doc("td", tr);
+			
+			let event_summary = `${this.doc(tr_fields[2]).text().trim()} ${this.doc(tr_fields[3]).text().trim()}`,
+				event_location = this.doc(tr_fields[8]).text().trim(),
 				event_organiser = {
-					name: tr.children()[9].text(),
+					name: this.doc(tr_fields[9]).text(),
 					email: "unknown@example.com"
 				};
-		    this.parse_week_expr(tr.children()[1].text()).map((week) => {
+		    this.parse_week_expr(this.doc(tr_fields[1]).text()).map((week) => {
 				// Calculate the start and end times of the session
-				let day = tr.children()[5].text().trim();
+				let day = this.doc(tr_fields[5]).text().trim();
 				
-				let start = tr.children()[6].trim().split(":").map((p) => parseInt(p));
+				let start = this.doc(tr_fields[6]).text().trim().split(":").map((p) => parseInt(p));
 				start = { hours: start[0], minutes: start[1] };
 				
-				let end = tr.children()[7].trim().split(":").map((p) => parseInt(p));
+				let end = this.doc(tr_fields[7]).text().trim().split(":").map((p) => parseInt(p));
 				end = { hours: end[0], minutes: end[1] };
 				
-				add_event({
+				this.add_event({
 					uid: crypto.createHash("sha1").update(`${event_summary} @ ${event_location} @ ${week}[${day}] ${start.hours}:${start.minutes}-${end.hours}:${end.minutes}`).digest("hex"),
 					start: this.week_number_to_date(week)
 						.day(day)
@@ -68,7 +89,7 @@ class TimetableParser
 					
 					alarms: [15, 0],
 					
-					summary: event_summary
+					summary: event_summary,
 					location: event_location,
 					organizer: event_organiser
 				});
@@ -112,7 +133,7 @@ class TimetableParser
 	}
 	
 	week_number_to_date(week) {
-		return this.week1.add(week, "weeks");
+		return this.week1.clone().add(week, "weeks");
 	}
 }
 
